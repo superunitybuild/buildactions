e4e978dbc3c1b375b43866f1fbe3f3c968625cba
Chase Pettit <chasepettit@gmail.com>
2017-01-10 17:18:17 -0500

    ScriptRunner.

---
+++ UnityBuild-ScriptRunner/Editor/ScriptRunnerPost.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace SuperSystems.UnityBuild
+{
+
+public class ScriptRunnerPost : PostBuildAction
+{
+    [FilePath(false, true, "Select program/script to run.")]
+    public string scriptPath = "";
+    public string scriptArguments = "";
+    public bool perBuild = false;
+
+    public override void Execute()
+    {
+        if (!perBuild)
+        {
+            ScriptRunnerUtility.RunScript(scriptPath, scriptArguments);
+        }
+    }
+
+    public override void PerBuildExecute(BuildReleaseType releaseType, BuildPlatform platform, BuildArchitecture architecture, BuildDistribution distribution, System.DateTime buildTime, ref BuildOptions options, string configKey, string buildPath)
+    {
+        if (perBuild)
+        {
+            string resolvedScriptPath = BuildProject.GenerateBuildPath(scriptPath, releaseType, platform, architecture, distribution, buildTime);
+            string resolvedScriptArgs = BuildProject.GenerateBuildPath(scriptArguments, releaseType, platform, architecture, distribution, buildTime);
+
+            ScriptRunnerUtility.RunScript(resolvedScriptPath, resolvedScriptArgs);
+        }
+    }
+}
+
+}
---
+++ UnityBuild-ScriptRunner/Editor/ScriptRunnerUtility.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace SuperSystems.UnityBuild
+{
+
+public static class ScriptRunnerUtility
+{
+    public static void RunScript(string scriptPath, string arguments)
+    {
+        ProcessStartInfo startInfo = new ProcessStartInfo();
+        startInfo.FileName = Path.GetFullPath(scriptPath);
+
+        if (!string.IsNullOrEmpty(arguments))
+            startInfo.Arguments = arguments;
+
+        Process proc = Process.Start(startInfo);
+        proc.WaitForExit();
+    }
+}
+
+}
