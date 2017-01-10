9fd359e872ae66c66d394e88a8708be3219ef828
Chase Pettit <chasepettit@gmail.com>
2017-01-10 13:45:23 -0500

    BuildAssetBundle implementation.

--- UnityBuild-AssetBundle/Editor/BuildAssetBundles.cs
+++ UnityBuild-AssetBundle/Editor/BuildAssetBundles.cs
@@ -5,63 +5,92 @@ using UnityEngine;
 namespace SuperSystems.UnityBuild
 {
 
-public sealed class BuildAssetBundles : PostBuildAction
+public sealed class BuildAssetBundles : PreBuildAction
 {
     #region Public Variables
 
+    [FilePath(true, true, "Select AssetBundle output directory.")]
     public string buildPath = Path.Combine("bin", "bundles");
+
+    public bool chunkBased = true;
+    public bool uncompressed = false;
+    public bool disableWriteTypeTree = false;
+    public bool ignoreTypeTreeChanges = false;
+    public bool forceRebuild = false;
+    public bool appendHash = false;
+    public bool strictMode = false;
 
     #endregion
 
     #region Public Methods
 
-    public override void Execute(BuildReleaseType releaseType, BuildPlatform platform, BuildArchitecture architecture, BuildDistribution distribution)
+    public override void PerBuildExecute(
+        BuildReleaseType releaseType,
+        BuildPlatform platform,
+        BuildArchitecture architecture,
+        BuildDistribution distribution,
+        System.DateTime buildTime, ref BuildOptions options, string configKey, string buildPath)
     {
-        Build(platform);
+        Build(platform, architecture);
     }
 
     public void BuildAll()
     {
-        //for (int i = 0; i < BuildProject.platforms.Count; i++)
-        //{
-        //    BuildPlatform platform = BuildProject.platforms[i];
-        //    Build(platform);
-        //}
+        string[] buildConfigs = BuildSettings.projectConfigurations.BuildAllKeychains();
+
+        for (int i = 0; i < buildConfigs.Length; i++)
+        {
+            BuildReleaseType releaseType;
+            BuildPlatform platform;
+            BuildArchitecture arch;
+            BuildDistribution dist;
+            string configKey = buildConfigs[i];
+
+            //BuildNotificationList.instance.AddNotification(new BuildNotification(
+            //    BuildNotification.Category.Notification,
+            //    string.Format("Building ({0}/{1}): ", i + 1, buildConfigs.Length), configKey,
+            //    true, null));
+
+            BuildSettings.projectConfigurations.ParseKeychain(configKey, out releaseType, out platform, out arch, out dist);
+            Build(platform, arch);
+        }
     }
 
     #endregion
 
     #region Private Methods
 
-    private void Build(BuildPlatform platform)
+    private void Build(BuildPlatform platform, BuildArchitecture arch)
     {
-        //if (!platform.buildEnabled)
-        //    return;
+        if (!platform.enabled || !arch.enabled)
+            return;
 
-        //// Path where this platform's AssetBundles will be built.
-        //string platformBundlePath = BuildAssetBundlesSettings.buildPath + Path.DirectorySeparatorChar + platform.name;
+        // Create options mask.
+        BuildAssetBundleOptions options = BuildAssetBundleOptions.None;
+        
+        if (uncompressed)
+            options |= BuildAssetBundleOptions.UncompressedAssetBundle;
+        if (disableWriteTypeTree)
+            options |= BuildAssetBundleOptions.DisableWriteTypeTree;
+        if (forceRebuild)
+            options |= BuildAssetBundleOptions.ForceRebuildAssetBundle;
+        if (ignoreTypeTreeChanges)
+            options |= BuildAssetBundleOptions.IgnoreTypeTreeChanges;
+        if (appendHash)
+            options |= BuildAssetBundleOptions.AppendHashToAssetBundleName;
+        if (chunkBased)
+            options |= BuildAssetBundleOptions.ChunkBasedCompression;
+        if (strictMode)
+            options |= BuildAssetBundleOptions.StrictMode;
+        
+        string platformBundlePath = Path.Combine(buildPath, Path.Combine(platform.platformName, arch.name));
 
-        //// Create build destination directory if it does not exist.
-        //if (!Directory.Exists(platformBundlePath))
-        //    Directory.CreateDirectory(platformBundlePath);
+        // Create build destination directory if it does not exist.
+        if (!Directory.Exists(platformBundlePath))
+            Directory.CreateDirectory(platformBundlePath);
 
-        //// Build AssetBundles.
-        //BuildPipeline.BuildAssetBundles(platformBundlePath, BuildAssetBundleOptions.None, platform.target);
-
-        //// Copy AssetBundles to platform's data directory.
-        //if (BuildAssetBundlesSettings.copyToBuild && Directory.Exists(platformBundlePath))
-        //{
-        //    string bundleDirectory = platform.dataDirectory + "Bundles" + Path.DirectorySeparatorChar;
-        //    string targetPath = bundleDirectory + platform.name;
-
-        //    if (Directory.Exists(bundleDirectory))
-        //        FileUtil.DeleteFileOrDirectory(bundleDirectory);
-
-        //    Directory.CreateDirectory(bundleDirectory);
-        //    FileUtil.CopyFileOrDirectory(platformBundlePath + Path.DirectorySeparatorChar, targetPath);
-        //}
-
-        string platformBundlePath = Path.Combine(buildPath, platform.platformName);
+        // Build AssetBundles.
+        BuildPipeline.BuildAssetBundles(platformBundlePath, options, arch.target);
     }
 
     #endregion
