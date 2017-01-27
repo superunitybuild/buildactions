f3e7cc77f5ad468ae091c53ee10313fc7e0d2631
Chase Pettit <chasepettit@gmail.com>
2017-01-26 20:34:51 -0500

    Minor itch argument changes.

--- UnityBuild-ItchUploader/Editor/UploadItch.cs
+++ UnityBuild-ItchUploader/Editor/UploadItch.cs
@@ -1,17 +1,24 @@
-﻿using UnityEngine;
-using UnityEditor;
+﻿using System.Diagnostics;
 using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
 
 namespace SuperSystems.UnityBuild
 {
 
-    public class UploadItch : BuildAction, IPostBuildAction, IPostBuildPerPlatformAction
+    public class UploadItch : BuildAction, IPostBuildPerPlatformAction
     {
+        private const string WINDOWS = "windows";
+        private const string OSX = "osx";
+        private const string LINUX = "linux";
+
         [FilePath(false, true, "Path to butler.exe")]
         public string pathToButlerExe = "";
         public string nameOfItchUser = "";
         public string nameOfItchGame = "";
-        [Header("The following field overrides the default channel name (the per-build architecture target), and will be applied to all builds. Use with care.")]
+
+        [Header("The following field overrides default channel name (the per-build architecture target), and will be applied to all builds. Use with care.")]
         public string itchChannelOverride = "";
 
         #region Public Methods
@@ -23,10 +30,28 @@ namespace SuperSystems.UnityBuild
                 UnityEngine.Debug.LogError("Couldn't find butler.exe file at path \"" + pathToButlerExe + "\", please check provided path");
                 return;
             }
-            string scriptArguments = "";
+
+            buildPath = Path.GetFullPath(buildPath);
+
+            StringBuilder scriptArguments = new StringBuilder("push ");
+
+            switch (architecture.target)
+            {
+                case BuildTarget.StandaloneOSXIntel:
+                case BuildTarget.StandaloneOSXIntel64:
+                case BuildTarget.StandaloneOSXUniversal:
+                case BuildTarget.StandaloneLinux:
+                case BuildTarget.StandaloneLinux64:
+                case BuildTarget.StandaloneLinuxUniversal:
+                    scriptArguments.Append("--fix-permissions ");
+                    break;
+            }
+
+            scriptArguments.Append("\"" + buildPath + "\"" + " " + nameOfItchUser + "/" + nameOfItchGame + ":");
+
             if (!string.IsNullOrEmpty(itchChannelOverride))
             {
-                scriptArguments = "push " + "\"" + buildPath + "\"" + " " + nameOfItchUser + "/" + nameOfItchGame + ":" + itchChannelOverride;
+                scriptArguments.Append(itchChannelOverride);
             }
             else
             {
@@ -35,24 +60,24 @@ namespace SuperSystems.UnityBuild
                 {
                     UnityEngine.Debug.LogWarning("UploadItch: The current BuildTarget doesn't appear to be a standard Itch.IO build target.");
                 }
-                scriptArguments = "push " + "\"" + buildPath + "\"" + " " + nameOfItchUser + "/" + nameOfItchGame + ":" + GetChannelName(architecture.target);
+
+                scriptArguments.Append(itchChannel);
             }
 
             //UnityEngine.Debug.Log("Would have run itch uploader with following command line: \"" + pathToButlerExe + " " + scriptArguments + "\"");
-            RunScript(pathToButlerExe, scriptArguments);
+            RunScript(pathToButlerExe, scriptArguments.ToString());
         }
 
         #endregion
 
         #region Private Methods
-        private const string WINDOWS = "windows";
-        private const string OSX = "osx";
-        private const string LINUX = "linux";
 
         private void RunScript(string scriptPath, string arguments)
         {
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.FileName = Path.GetFullPath(scriptPath);
+            startInfo.UseShellExecute = true;
+            startInfo.CreateNoWindow = false;
 
             if (!string.IsNullOrEmpty(arguments))
                 startInfo.Arguments = arguments;
