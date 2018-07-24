e32798a8652de091ee8579ea60465966fb736c12
Emre Solak <s.emresolak@dreamharvesters.com>
2018-07-24 16:23:35 +0300

    added update ios build number

---
+++ UnityBuild-BuildParameters/Editor/UpdateIOSBuildNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SuperSystems.UnityBuild;
+using UnityEditor;
+
+namespace DefaultNamespace
+{
+    public class UpdateIOSBuildNumber : BuildAction, IPreBuildPerPlatformAction
+    {
+        public override void PerBuildExecute(BuildReleaseType releaseType, BuildPlatform platform, BuildArchitecture architecture,
+            BuildDistribution distribution, DateTime buildTime, ref BuildOptions options, string configKey, string buildPath)
+        {
+            if (platform.targetGroup == BuildTargetGroup.iOS)
+            {
+                PlayerSettings.iOS.buildNumber = BuildSettings.productParameters.buildCounter.ToString();
+            }
+        }
+    }
+}
