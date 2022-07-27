1f85e142332aa7ee6bed9f1c1f582d3361eb6a5b
technodragon253 <70380275+technodragon253@users.noreply.github.com>
2022-07-26 18:39:31 -0600

    Added $BUILDPATH and $BASEPATH support to scriptrunner.cs

--- Editor/ScriptRunner/ScriptRunner.cs
+++ Editor/ScriptRunner/ScriptRunner.cs
@@ -18,8 +18,15 @@ namespace SuperUnityBuild.BuildActions
 
         public override void PerBuildExecute(BuildReleaseType releaseType, BuildPlatform platform, BuildArchitecture architecture, BuildDistribution distribution, System.DateTime buildTime, ref BuildOptions options, string configKey, string buildPath)
         {
-            string resolvedScriptPath = BuildProject.ResolvePath(scriptPath, releaseType, platform, architecture, distribution, buildTime);
-            string resolvedScriptArgs = BuildProject.ResolvePath(scriptArguments, releaseType, platform, architecture, distribution, buildTime);
+            string _scriptPath = (string)scriptPath.Clone();
+            _scriptPath = _scriptPath.Replace("$BUILDPATH", buildPath);
+            _scriptPath = _scriptPath.Replace("$BASEPATH", BuildSettings.basicSettings.baseBuildFolder);
+            string resolvedScriptPath = BuildProject.ResolvePath(_scriptPath, releaseType, platform, architecture, distribution, buildTime);
+
+            string _scriptArguments = (string)scriptArguments.Clone();
+            _scriptArguments = _scriptArguments.Replace("$BUILDPATH", buildPath);
+            _scriptArguments = _scriptArguments.Replace("$BASEPATH", BuildSettings.basicSettings.baseBuildFolder);
+            string resolvedScriptArgs = BuildProject.ResolvePath(_scriptArguments, releaseType, platform, architecture, distribution, buildTime);
 
             RunScript(resolvedScriptPath, resolvedScriptArgs);
         }
