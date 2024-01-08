65a9c6dc4ff7e99a553a5a6851dde17e7916887e
cosformula <cosformula@gmail.com>
2024-01-08 15:00:21 +0800

    fix: fixing zipped Mac app permissions

--- Editor/ZipFile/ZipFileOperation.cs
+++ Editor/ZipFile/ZipFileOperation.cs
@@ -1,12 +1,16 @@
-﻿using Ionic.Zip;
-using SuperUnityBuild.BuildTool;
+﻿using SuperUnityBuild.BuildTool;
 using System;
 using System.IO;
-using UnityEngine;
+#if NET_4_6 || NETSTANDARD2_0_OR_GREATER
+using System.IO.Compression;
+#else
+using Ionic.Zip;
+#endif
 
 namespace SuperUnityBuild.BuildActions
 {
-    public class ZipFileOperation : BuildAction, IPreBuildAction, IPreBuildPerPlatformAction, IPostBuildAction, IPostBuildPerPlatformAction
+
+    public class CustomZipFileOperation : BuildAction, IPreBuildAction, IPreBuildPerPlatformAction, IPostBuildAction, IPostBuildPerPlatformAction
     {
         public string inputPath = "$BUILDPATH";
         public string outputPath = "$BUILDPATH";
@@ -22,10 +26,9 @@ namespace SuperUnityBuild.BuildActions
             if (!resolvedOutputPath.EndsWith(".zip"))
                 resolvedOutputPath += ".zip";
 
-            PerformZip(Path.GetFullPath(resolvedInputPath), Path.GetFullPath(resolvedOutputPath));
+            PerformZip(Path.GetFullPath(resolvedInputPath), Path.GetFullPath(resolvedOutputPath), platform);
         }
-
-        private void PerformZip(string inputPath, string outputPath)
+        private void PerformZip(string inputPath, string outputPath, BuildPlatform platform)
         {
             try
             {
@@ -48,14 +51,27 @@ namespace SuperUnityBuild.BuildActions
                 // Delete old file if it exists.
                 if (File.Exists(outputPath))
                     File.Delete(outputPath);
-
-                using (ZipFile zip = new ZipFile(outputPath))
+#if NET_4_6 || NETSTANDARD2_0_OR_GREATER
+        System.IO.Compression.ZipFile.CreateFromDirectory(inputPath, outputPath);
+        if (platform.platformName == "macOS")
+        {
+          var zipArchive = System.IO.Compression.ZipFile.Open(outputPath, ZipArchiveMode.Update);
+          foreach (var entry in zipArchive.Entries)
+          {
+            if (entry.FullName.Contains("Contents/MacOS"))
+              entry.ExternalAttributes = 0755;
+          }
+          zipArchive.Dispose();
+        }
+#else
+                using (Ionic.Zip.ZipFile zip = new Ionic.Zip.ZipFile(outputPath))
                 {
                     zip.ParallelDeflateThreshold = -1; // Parallel deflate is bugged in DotNetZip, so we need to disable it.
                     zip.UseZip64WhenSaving = Zip64Option.AsNecessary;
                     zip.AddDirectory(inputPath);
                     zip.Save();
                 }
+#endif
             }
             catch (Exception ex)
             {
